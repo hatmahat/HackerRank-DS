a74d267581bf14a623004f60a93ad6c7bd69588b
hatmahat <56577748+hatmahat@users.noreply.github.com>
2020-06-10 11:26:31 +0700

    Update 1. Arrays - DS.py

--- 1. Arrays - DS.py
+++ 1. Arrays - DS.py
@@ -1,7 +1,12 @@
 N = int(input().strip())
 arr = input().strip().split()
 
+print(" ".join(arr[::-1])) # new version
+
+'''
+# Old verison
 out = ""
 for i in arr[::-1]:
     out += str(i) + " "
 print(out.strip())
+'''
